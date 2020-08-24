726b8abe42fe3bd77926d4f1196192e3e5850d68
Bechet <yuukikune@gmail.com>
2020-08-24 23:38:55 +0200

    Adding listLevel with slimes, unclicable for the moment

---
+++ lib/bean/level.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Level {
+  String name;
+  List<bool> listChallenge;
+
+  Level({this.name, this.listChallenge});
+}
--- lib/screens/listlevel.dart
+++ lib/screens/listlevel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pegii_app/bean/level.dart';
 
 class ListLevel extends StatefulWidget {
   @override
@@ -6,6 +7,14 @@ class ListLevel extends StatefulWidget {
 }
 
 class _ListLevelState extends State<ListLevel> {
+
+  final List<Level> listLevel = [
+    Level(name: "red", listChallenge: [false, false]),
+    Level(name: "green", listChallenge: [false, false]),
+    Level(name: "black", listChallenge: [true, false]),
+    Level(name: "white", listChallenge: [false, false]),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +23,39 @@ class _ListLevelState extends State<ListLevel> {
         centerTitle: true,
         backgroundColor: Colors.red,
       ),
-      body: Text("ListLevel"),
+      body: Column(
+        children: listLevel.map((level) => levelCard(level)).toList(),
+      )
+    );
+
+
+  }
+
+  Widget levelCard(Level level) {
+    return Card(
+      margin: EdgeInsets.all(20.0),
+      child: Row(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.lightBlueAccent,
+                backgroundImage: AssetImage('assets/characters/slime_${level.name}.png'),
+                radius: 20.0,
+              ),
+              Text(level.name),
+            ],
+          ),
+          Row(
+            children: level.listChallenge.map((challenge) {
+              return Icon(
+                challenge ? Icons.star : Icons.star_border,
+                color: Colors.orangeAccent,
+              );
+            }).toList(),
+          ),
+        ],
+      )
     );
   }
 }
